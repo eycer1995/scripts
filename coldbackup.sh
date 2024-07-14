47102ccde88f17f84c582a98d16fc5fa0ca7751d
Eycer Leon <eycer1995@gmail.com>
2024-07-14 15:55:35 -0300

    update for mediastage
    worked fine on ares HDD, removed movies folder got too big

--- coldbackup.sh
+++ coldbackup.sh
@@ -14,8 +14,8 @@ gitea_backup() {
 }
 
 # Backup privseedbox
-privseedbox_backup() {
-	rsync -saP privseedbox:/var/lib/transmission-daemon/downloads /mnt/ares/privseedbox
+seedbox_backup() {
+	rsync -saP seedbox:/var/lib/transmission-daemon/downloads /mnt/ares/privseedbox
 
 }
 
@@ -26,12 +26,29 @@ mam_backup() {
 
 # Backup jellyfin files and media
 jellyfin_backup() {
-	ROOT_JELLYFIN="/mnt/hades/hms/jellyfin"
-	rsync -aP rpi01:$ROOT_JELLYFIN/animovies /mnt/ares/jellyfin
-	rsync -aP rpi01:$ROOT_JELLYFIN/config /mnt/ares/jellyfin
-	rsync -aP rpi01:$ROOT_JELLYFIN/movies /mnt/ares/jellyfin
-	rsync -aP rpi01:$ROOT_JELLYFIN/tvseries /mnt/ares/jellyfin
-	rsync -aP rpi01:$ROOT_JELLYFIN/kdrama /mnt/ares/jellyfin
+	ROOT_JELLYFIN="/opt/hms/jellyfin"
+	rsync -aP mediastage:$ROOT_JELLYFIN/animovies /mnt/ares/jellyfin
+	rsync -aP mediastage:$ROOT_JELLYFIN/config /mnt/ares/jellyfin
+	# movies are too heavy 238G wont fit on ares HDD
+	#rsync -aP mediastage:$ROOT_JELLYFIN/movies /mnt/ares/jellyfin
+	rsync -aP mediastage:$ROOT_JELLYFIN/tvseries /mnt/ares/jellyfin
+	# kdrama gets too heavy and repeated from seedbox backup
+	#rsync -aP mediastage:$ROOT_JELLYFIN/kdrama /mnt/ares/jellyfin
+
+    ssh mediastage "ls /opt/hms/jellyfin/anime" > /mnt/ares/anime.txt
+    ssh mediastage "ls /opt/hms/jellyfin/animovies" > /mnt/ares/animovies.txt
+    ssh mediastage "ls /opt/hms/jellyfin/movies" > /mnt/ares/movies.txt
+}
+
+hms_config_backup() {
+	ROOT_HMS="/opt/hms"
+	#bazarr  jackett jellyseerr  radarr  sonarr  tvsonarr
+	sudo rsync -aP -e "ssh -F /home/eycer/.ssh/config" --rsync-path="sudo rsync" mediastage:$ROOT_HMS/bazarr /mnt/ares
+	sudo rsync -aP -e "ssh -F /home/eycer/.ssh/config" --rsync-path="sudo rsync" mediastage:$ROOT_HMS/jackettt /mnt/ares
+	sudo rsync -aP -e "ssh -F /home/eycer/.ssh/config" --rsync-path="sudo rsync" mediastage:$ROOT_HMS/jellyseerr /mnt/ares
+	sudo rsync -aP -e "ssh -F /home/eycer/.ssh/config" --rsync-path="sudo rsync" mediastage:$ROOT_HMS/radarr /mnt/ares
+	sudo rsync -aP -e "ssh -F /home/eycer/.ssh/config" --rsync-path="sudo rsync" mediastage:$ROOT_HMS/sonarr /mnt/ares
+	sudo rsync -aP -e "ssh -F /home/eycer/.ssh/config" --rsync-path="sudo rsync" mediastage:$ROOT_HMS/tvsonarr /mnt/ares
 }
 
 kavita_backup() {
@@ -43,6 +60,7 @@ kavita_backup() {
 if sudo blkid -s UUID -o value /dev/sdd1 | grep -q "$ARES_UUID"; then
   # Check if /mnt/ares is already mounted, if not mount it.
   df -h /mnt/ares
+  sudo mount -U $ARES_UUID /mnt/ares
   if [ $? -eq 0 ]; then
 	  echo "HDD ares is already mounted"
   else
@@ -52,11 +70,12 @@ if sudo blkid -s UUID -o value /dev/sdd1 | grep -q "$ARES_UUID"; then
   fi
   home_backup
   gitea_backup
-  privseedbox_backup
+  seedbox_backup
   mam_backup
   jellyfin_backup
+  hms_config_backup
 
-elif sudo blkid -s UUID -o value /dev/sdd1 | grep -q "$EROS_UUID"; then
+elif sudo blkid -s UUID -o value /dev/sde1 | grep -q "$EROS_UUID"; then
   # Check if /mnt/eros is already mounted, if not mount it.
   df -h /mnt/eros
   if [ $? -eq 0 ]; then
